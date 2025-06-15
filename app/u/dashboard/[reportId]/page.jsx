da0a7769b99c716218c7b6fbe387f017a37d4a54
AAYUSH SINGH <aayush3708@outlook.com>
2025-06-15 13:45:09 +0530

    deploy fix

--- app/u/dashboard/[reportId]/page.jsx
+++
@@ -1,5 +0,0 @@
-import { LabReport } from "../../../components/LabReport";
-
-export function Display({ metrics, remarks }) {
-  return <LabReport metrics={metrics} remarks={remarks}></LabReport>;
-}
